ef8666757c9e3554fe56a23a76c006fc8026def6
amacut <adamacut7@gmail.com>
2021-03-15 15:31:19 +0100

    updated component: rentals-history

--- src/app/rentals-history/rentals-history.component.ts
+++ src/app/rentals-history/rentals-history.component.ts
@@ -10,62 +10,26 @@ import {FortAwesomeService} from '../shared/fort-awesome/fort-awesome.service';
 })
 export class RentalsHistoryComponent implements OnInit {
 
-  rentDetailsIsShow = false;
-  rentalsList: Rental[] = [];
-  /*  rentalsList: Rental[] = [
-      {
-        id: 1,
-        rentDate: '2020-11-15 16:00:00',
-        returnDate: '2020-11-15 18:00:00',
-        drivingTime: '01:50:00',
-        drivingPrice: 55.0,
-        distance: 10.0,
-        distancePrice: 8.0,
-        stopOverTime: '00:10:00',
-        stopOverPrice: 2.0,
-        totalPrice: 65.0,
-        isActive: false,
-      },
-      {
-        id: 2,
-        rentDate: '2020-11-16 16:00:00',
-        returnDate: '2020-11-16 19:00:00',
-        drivingTime: '02:50:00',
-        drivingPrice: 55.0,
-        distance: 10.0,
-        distancePrice: 8.0,
-        stopOverTime: '00:10:00',
-        stopOverPrice: 2.0,
-        totalPrice: 75.0,
-        isActive: false,
-      },
-      {
-        id: 3,
-        rentDate: '2020-11-17 10:00:00',
-        returnDate: '2020-11-17 14:00:00',
-        drivingTime: '03:50:00',
-        drivingPrice: 55.0,
-        distance: 10.0,
-        distancePrice: 8.0,
-        stopOverTime: '00:10:00',
-        stopOverPrice: 2.0,
-        totalPrice: 85.0,
-        isActive: false,
-      }
-    ];*/
-  chosenRent: RentalDetails = null;
-
   constructor(private service: RentalsService,
               private cookies: CookieService,
               private icons: FortAwesomeService) {
   }
 
+  rentDetailsIsShow = false;
+  rentalsList: Rental[] = [];
+
+  chosenRent: RentalDetails = null;
+
+  carIcon = this.icons.faCar;
+  sortIcon = this.icons.faSort;
+  detailsIcon = this.icons.faInfo;
+  private id = this.cookies.get('id');
+  key = 'id';
+  p = 1;
+  reverse = true;
   ngOnInit(): void {
     this.getRentals();
   }
-
-  carIcon = this.icons.faCar;
-  private id = this.cookies.get('id');
 
   getRentals(): void {
     this.service.getAllRentals(this.id).subscribe(
@@ -91,6 +55,10 @@ export class RentalsHistoryComponent implements OnInit {
   hideDetails(): void {
     document.querySelector('.modal-wrap').classList.remove('active');
     document.querySelector('.app-content').classList.remove('blur');
+  }
+  sort(key): void {
+    this.key = key;
+    this.reverse = !this.reverse;
   }
 }
 
