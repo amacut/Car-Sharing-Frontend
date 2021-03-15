548e6cdd12b3f9f1c2774bcf6ed375d7e3f95123
amacut <adamacut7@gmail.com>
2021-03-15 15:28:18 +0100

    added model: rental

---
+++ src/app/model/rental.ts
@@ -0,0 +1,14 @@
+export class Rental {
+userId: string;
+origin: string;
+destination: string;
+drivingTime: number;
+distance: number;
+stopOverTime: number;
+vehicleId: number;
+latitude: number;
+longitude: number;
+
+constructor() {
+}
+}
