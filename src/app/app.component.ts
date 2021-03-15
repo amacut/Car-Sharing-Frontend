683a9972eb1894054ae48e3907bbb3f089343850
amacut <adamacut7@gmail.com>
2021-03-15 15:36:11 +0100

    updated app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Car Sharing App';
+
+  userPosition = new UserPosition();
+}
+export class UserPosition {
+  private _latitude: number;
+  private _longitude: number;
+
+  constructor() {
+  }
+
+
+  get latitude(): number {
+    return this._latitude;
+  }
+
+  set latitude(value: number) {
+    this._latitude = value;
+  }
+
+  get longitude(): number {
+    return this._longitude;
+  }
+
+  set longitude(value: number) {
+    this._longitude = value;
+  }
 }
 
 
